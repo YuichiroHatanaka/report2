e2b030c085bbebe1cc49cbc1254584ed7043c440
YuichiroHatanaka <73636802+YuichiroHatanaka@users.noreply.github.com>
2020-11-20 22:12:29 +0900

    Update mondai1.c

--- mondai1.c
+++ mondai1.c
@@ -2,17 +2,26 @@
 #include <ctype.h>
  
 int main(){
+    //ファイル構造体へのポインタを宣言
+    //読込み用、書き込み用それぞれ宣言する
     FILE *lf,*sf;
     int c;
  
+    //読み込み、書込みそれぞれでオープン
+    //短縮して書いていますが、
+    //if(lf=fopen("1.c","r")==NULL){}
+    //if(sf=fopen("test.c","w")==NULL){}
+    //の場合と同じです。
     if(!(lf=fopen("1.c","r")) || (!(sf=fopen("test.c","w")))){
-     //1.c test.cはファイル名
         printf(" ファイルオープンエラー\n");
         return -1;
     }
  
+    //ファイルの中身がEOFになるまで続ける
+    //ファイルから一文字読み込みcへ格納
     while((c=fgetc(lf))!=EOF){
-        //fputc(c,sf);
+        //ファイルに出力
+	
     if(isalpha(c)){
       fputc(c,sf);
     }else if(c == ' '){
@@ -23,8 +32,12 @@ int main(){
       fputc(' ',sf);
     }
     }
+ 
+    //書込みクローズ
     fclose(sf);
+    //読み込みクローズ
     fclose(lf);
  
     return 0;
 }
+
