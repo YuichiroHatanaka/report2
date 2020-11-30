ae778ff043957e0c9e5ef0f5d3e2c0de037f08de
YuichiroHatanaka <73636802+YuichiroHatanaka@users.noreply.github.com>
2020-11-30 22:37:47 +0900

    Update monndai3.c

--- monndai3.c
+++ monndai3.c
@@ -1,13 +1,12 @@
 #include <stdio.h>
 #include <ctype.h>
-#<inckude <stdlib.h>
+#include <stdlib.h>
 
 int x;
-int i;
+int i, j;
 int c;
 int M;
 int a;
-int p = 0;
 int count[26];
 int GetRandom(int min, int max);
 FILE *lf;
@@ -22,19 +21,24 @@ int main(void){
     for(i = 0;i < 26;i++){
       if(c == 65 + i){
         count[i] = count[i] + 1;
-      }
-    }
+       }
+     }
   }
   for(i = 0;i < 26;i++){
     M = M + count[i];
   }
+//printf("%d\n",M);
   for(i = 0;i < 100;i++){
     a = GetRandom(1,M);
+//printf("ランダム = %d",a);
+    int p = 0;
     for(j = 0;j < 26;j++){
-      if(p < a && a <= p + count[j]){
+      if(p <  a && a <= p + count[j]){
         printf("%c", 65+j);
-        p = p + count[j];
-      }
-    }
+        }
+    p = p + count[j];
+     }
+   }
+  printf("\n");
   fclose(lf);
 }
